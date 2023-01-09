ac68f84f9083ab3e8a3e379832590af2c826ce9d
Jardel Mandel Cardoso <jardel.mandel@gmail.com>
2023-01-09 11:32:43 -0300

    Add project files.

---
+++ Operadores/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Operadores artimeticos
+            //int num1 = 10;
+            //int num2 = 2;
+            //int resultado = num1 + num2;
+
+            //Console.WriteLine(num1 + num2);
+            //Console.WriteLine(num1 - num2);
+            //Console.WriteLine(num1 * num2);
+            //Console.WriteLine(num1 / num2);
+            //Console.WriteLine(num1 % num2);
+            #endregion
+
+            #region Prioridade de operação
+            double num1 = 100;
+            double num2 = 10;
+            double num3 = 5;
+
+            double res = (num1 + num2) * (num3 / 3);
+
+            Console.WriteLine(res);
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
